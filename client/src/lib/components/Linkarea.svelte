bd8a745ef5951d8b55621edd4c5880358b0bf096
Denzel <denzel.mitski@gmail.com>
2025-07-31 01:01:32 +0200

    style: change link area

--- client/src/lib/components/Linkarea.svelte
+++ client/src/lib/components/Linkarea.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-	let text = $state("");
-	let isFocused = $state(false);
+	let { inputText = $bindable(), areaFocused = $bindable() } = $props();
+	function handleBlur() {
+		areaFocused = false;
+	}
+	function handleFocus() {
+		areaFocused = true;
+	}
 </script>
 
 <div class="font-main relative text-white">
-	<textarea
-		class="bg-medal-black outline-medal-lgray h-52 w-full resize-none overflow-y-auto rounded-2xl px-4 py-2 font-mono outline-2 focus:outline-2"
-		id="input"
-		bind:value={text}
-		bind:focused={isFocused}
-	></textarea>
-	{#if !text && !isFocused}
-		<label class="absolute top-2 left-4 font-bold" for="input">
+	<div
+		class="bg-medal-black outline-medal-lgray focus-within:outline-medal-orange h-60 w-full rounded-2xl p-4 outline-2 transition duration-200 ease-in-out focus-within:outline-2"
+	>
+		<textarea
+			class="h-full w-full resize-none overflow-x-auto overflow-y-auto bg-transparent font-mono whitespace-nowrap text-white outline-none"
+			style="scrollbar-width: thin; scrollbar-color: #4b5563 transparent;"
+			id="input"
+			bind:value={inputText}
+			bind:focused={areaFocused}
+			onblur={handleBlur}
+			onfocus={handleFocus}
+		></textarea>
+	</div>
+	{#if !inputText && !areaFocused}
+		<label class="pointer-events-none absolute top-4 left-4 font-bold" for="input">
 			Paste your <span class="text-medal-lime">links</span> here, separated by a line
 		</label>
 	{/if}
